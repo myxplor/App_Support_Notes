f640d5bbef537ef7963f8a42ae4da9b25f461395
Thomas Dalton-Carwood <tdalton-carwood@qktech.com.au>
2021-07-16 16:45:53 +1000

    more git ignore cleanup and ini migration

---
+++ api/NotesAPI/Migrations/20210716064247_initNotesTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotesAPI.Migrations
+{
+    public partial class initNotesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
---
+++ api/NotesAPI/Models/Notes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Models {
+    public class Notes {
+
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [Required]
+        public string Note { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        [Required]
+        public DateTime LastModified { get; set; }
+
+    }
+}
