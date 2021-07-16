b4fa3dd593163133e0311b61ef5e6fb8e3d41c42
Thomas Dalton-Carwood <tdc_93@hotmail.com>
2021-07-16 16:13:29 +1000

    Init commit. Create and Get API calls created. Model and dbContext set up.

---
+++ api/NotesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesAPI.Data {
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+
+        //Tables go here
+        public DbSet<Notes> Notes { get; set; }
+    }
+}
